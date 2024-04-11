b6d747c82b8c040259219adbdff0aa62386ecdf9
DYNOH812 <munyuadennis@gmail.com>
2024-04-11 09:59:55 +0300

    new updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
   title: 'property search | Find the perfect rental',
@@ -12,6 +13,7 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
+    <AuthProvider>
     <html lang='en'>
       <body>
        <Navbar/>
@@ -19,6 +21,7 @@ const MainLayout = ({ children }) => {
        <Footer/>
       </body>
     </html>
+    </AuthProvider>
   )
 }
 
