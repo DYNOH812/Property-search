23be1e1dcd970f4af3cb10ccf3c99b9c5af6f5f5
DYNOH812 <munyuadennis@gmail.com>
2024-04-23 16:50:40 +0300

    worked on the search

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react'; 
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/request';
-import { ClipLoader } from 'react-spinners';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 const override = {
   display: 'block',
@@ -34,29 +34,29 @@ const PropertiesPage = () => {
     properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   }, [properties]);
 
-  return (
-    <section className="px-4 py-6">
+  
+
+  return (<>
+    <section className="bg-blue-700 py-4">
+      <div className="max-w-7xl mx-auto px-4 flex-col items-start sm:px-6 lg:px-8  ">
+        <PropertySearchForm/>
+      </div>
+    </section>
+   
+        <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        {loading ? (
-            <ClipLoader
-            color='#3b82f6'
-            loading={loading}
-            cssOverride={override}
-            size={150}
-            aria-label="Loading Spinner"
-          />
-        ) : properties.length === 0 ? (
-          <p>No Properties found</p>
-        ) : (
+      
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
-        )}
+        
       </div>
     </section>
-  );
+      
+    
+    </>);
 };
 
 export default PropertiesPage;
